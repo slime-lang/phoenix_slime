4e6a39e773c948f430eec4a0068047a889dea721
Chris McCord <chris@chrismccord.com>
2015-06-17 23:49:32 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PhoenixHaml.Mixfile do
   def project do
     [
       app: :phoenix_haml,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.0.1 or ~> 1.1",
       deps: deps,
       package: [
