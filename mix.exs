99efc04b0e3f5ae12a4c6100b1889ca0ba2f79d6
Sean Callan <seancallan@gmail.com>
2015-08-05 07:56:14 -0700

    Bump slim_fast version

--- mix.exs
+++ mix.exs
@@ -1,33 +1,35 @@
 defmodule PhoenixSlim.Mixfile do
   use Mix.Project
 
+  @version "0.2.0"
+
   def project do
     [
       app: :phoenix_slim,
-      version: "0.1.0",
-      elixir: "~> 1.0",
       deps: deps,
-      package: [
-        contributors: ["Sean Callan"],
-        licenses: ["MIT"],
-        links: %{github: "https://github.com/doomspork/phoenix_slim"}
-      ],
       description: """
       Phoenix Template Engine for Slim
-      """
-    ]
+      """,
+      elixir: "~> 1.0",
+      package: package,
+      version: @version]
   end
 
   def application do
     [applications: [:phoenix, :slim_fast]]
   end
 
-  defp deps do
-    [
-      {:phoenix, "~> 0.15.0"},
-      {:phoenix_html, "~> 2.0"},
-      {:cowboy, "~> 1.0", only: [:dev, :test]},
-      {:slim_fast, "~> 0.2.0"}
-    ]
+  def deps do
+    [{:phoenix, "~> 0.15.0"},
+     {:phoenix_html, "~> 2.0"},
+     {:cowboy, "~> 1.0", only: [:dev, :test]},
+     {:slim_fast, "~> 0.3.0"}]
+  end
+
+  defp package do
+    [contributors: ["Sean Callan"],
+     files: ["lib", "mix.exs", "README*", "LICENSE*"],
+     licenses: ["MIT"],
+     links: %{github: "https://github.com/doomspork/phoenix_slim"}]
   end
 end
