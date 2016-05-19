f7a7c7d3f56ef1434546cc3bbdefd38def84066e
Jay Hayes <jay@bignerdranch.com>
2016-05-19 11:09:39 -0500

    Fix typo in test description (#18)
    The template is written in slim, not haml

--- test/phoenix_slime_test.exs
+++ test/phoenix_slime_test.exs
@@ -16,7 +16,7 @@ defmodule PhoenixSlimeTest do
     assert html == {:safe, [[["" | "<html><body>"], "" | "<h2>New Template</h2>"] | "</body></html>"]}
   end
 
-  test "render a haml template without layout" do
+  test "render a slim template without layout" do
     html = View.render(MyApp.PageView, "new.html", [])
     assert html == {:safe, ["" | "<h2>New Template</h2>"]}
   end
