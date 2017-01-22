ebded67264ff01a171a85b24b8fd63166054379a
Anton Chuchkalov <a2new@yandex.ru>
2017-01-22 15:01:51 +0300

    fix Elixir 1.4 warnings

--- lib/mix/tasks/phoenix.gen.html.slime.ex
+++ lib/mix/tasks/phoenix.gen.html.slime.ex
@@ -90,7 +90,7 @@ defmodule Mix.Tasks.Phoenix.Gen.Html.Slime do
   defp validate_args!([_, plural | _] = args) do
     cond do
       String.contains?(plural, ":") ->
-        raise_with_help
+        raise_with_help()
       plural != Phoenix.Naming.underscore(plural) ->
         Mix.raise "Expected the second argument, #{inspect plural}, to be all lowercase using snake_case convention"
       true ->
@@ -99,7 +99,7 @@ defmodule Mix.Tasks.Phoenix.Gen.Html.Slime do
   end
 
   defp validate_args!(_) do
-    raise_with_help
+    raise_with_help()
   end
 
   defp raise_with_help do
