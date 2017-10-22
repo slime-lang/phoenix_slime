eabb40c6c296cd7576eaf8b48920b6940e946c36
Jonathan Mast <jon@jonmast.com>
2017-10-22 09:11:40 -0400

    Add phx.gen.layout.slime task
    Use phoenix 1.3 naming convention for mix task and place the generated
    file in the new template location.

---
+++ lib/mix/tasks/phx.gen.layout.slime.ex
@@ -0,0 +1,32 @@
+defmodule Mix.Tasks.Phx.Gen.Layout.Slime do
+  use Mix.Task
+
+  @shortdoc "Generates a default Phoenix layout file in Slime"
+
+  @moduledoc """
+  Generates a Phoenix layout file in Slime.
+
+      mix phx.gen.layout.slime
+
+  """
+  def run(_args) do
+    context_app = Mix.Phoenix.context_app()
+    web_prefix = Mix.Phoenix.web_path(context_app)
+    binding = [application_module: Mix.Phoenix.base()]
+
+    extension = PhoenixSlime.ConfiguredExtension.file_extension
+    Mix.Phoenix.copy_from(slime_paths(), "priv/templates/phoenix.gen.layout.slime", binding, [
+      {:eex, "app.html.eex", "#{web_prefix}/templates/layout/app.html.#{extension}"}
+    ])
+
+    instructions = """
+
+    A new #{web_prefix}/templates/layout/app.html.#{extension} file was generated.
+    """
+    Mix.shell.info instructions
+  end
+
+  defp slime_paths do
+    [".", :phoenix_slime]
+  end
+end
---
+++ test/mix/tasks/phx.gen.layout.slime_test.exs
@@ -0,0 +1,41 @@
+Code.require_file "../mix_helper.exs", __DIR__
+
+defmodule Mix.Tasks.Phx.Gen.Layout.SlimeTest do
+  use ExUnit.Case
+  import MixHelper
+
+  setup do
+    Mix.Task.clear
+    :ok
+  end
+
+  test "generates a slime layout file" do
+    in_tmp "generates slime layout file", fn ->
+      Mix.Tasks.Phx.Gen.Layout.Slime.run []
+
+      assert_file "lib/phoenix_slime_web/templates/layout/app.html.slime", fn file ->
+        assert file =~ "Hello PhoenixSlime"
+        assert file =~ "p.alert.alert-info"
+        assert file =~ "p.alert.alert-danger"
+      end
+
+    end
+  end
+
+  describe "when :use_slim_extension env is set to true" do
+    setup do
+      Application.put_env(:phoenix_slime, :use_slim_extension, true)
+      on_exit fn ->
+        Application.delete_env(:phoenix_slime, :use_slim_extension)
+      end
+      :ok
+    end
+
+    test "generates a file with .slim extension " do
+      in_tmp "generates .slim", fn ->
+        Mix.Tasks.Phx.Gen.Layout.Slime.run []
+        assert File.exists? "lib/phoenix_slime_web/templates/layout/app.html.slim"
+      end
+    end
+  end
+end
